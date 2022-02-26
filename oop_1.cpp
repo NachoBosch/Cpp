79162fa77b73ab588edd4b25fcd2385712d35f15
Nacho <nachobosch5@gmail.com>
2022-02-26 18:45:22 -0300

    Subo actualizaciones

--- oop_1.cpp
+++ oop_1.cpp
@@ -4,6 +4,12 @@ In cpp we have three access modifiers: private, public, protected
 Private is by default in every class
 A Method is useful for describe a behaviour of the object.
 For construct an object many times with the same variables we use Constructor
+To build a constructor there is 3 rules: 
+	1. No return type
+	2. The constructor has the same name that the class that belong it
+	3. The constructor must be public (but not always)
+	When we decide to create a constructor will be delete it the default constructor
+	
 */
 #include <iostream>
 #include <string>
@@ -15,7 +21,7 @@ class Employee{
 public:
 	string Name;
 	string Company;
-	int Age;
+	int Age=0;
 
 	void data(){
 
@@ -25,17 +31,23 @@ public:
 		cout<<"Company: "<<Company<<endl;
 		cout<<"Age: "<<Age<<endl;
 	}
+/*private:*/
+	Employee(string name, string company, int age){
+		Name = name;
+		Company = company;
+		Age = age;
+	}
 };
 int main(){
-	Employee employee1;
-	employee1.Name = "Nacho";
+	Employee employee1=Employee("Nacho","cys",29);
+	/*employee1.Name = "Nacho";
 	employee1.Company = "YT-CodeBeauty";
-	employee1.Age = 29;
+	employee1.Age = 29;*/
 	employee1.IntroduceYourself();
-	Employee employee2;
-	employee2.Name = "John";
+	Employee employee2=Employee("john","amazon",28);
+	/*employee2.Name = "John";
 	employee2.Company = "Amazon";
-	employee2.Age = 30;
+	employee2.Age = 30;*/
 	employee2.IntroduceYourself();
 
 }
